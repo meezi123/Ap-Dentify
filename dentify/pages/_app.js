7845ecf85fedf7d56071daae7c57752d077bc47b
Mamoona-Mahmood <mamoona.mahmood@protonmail.com>
2024-11-29 23:33:25 +0500

    Login authentication/ SignUp

--- dentify/pages/_app.js
+++ dentify/pages/_app.js
@@ -1,14 +1,15 @@
 import Navbar from "@/components/Navbar";
+import { SessionProvider } from "next-auth/react"
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return(
 
     <>
+    <SessionProvider>
     <Navbar/>
     <Component {...pageProps} />;
-    
- 
+    </SessionProvider>
     </>
   );
   
--- dentify/react-components/Login.js
+++ dentify/react-components/Login.js
@@ -1,21 +1,45 @@
 import { useRef } from "react";
 import styles from "./Login.module.css";
+import { signIn } from "next-auth/react";
 
 function loginUser(){
   //api call
 }
-function handler(){
-  //login input handle + navigation
-}
+
 
 const Login = () => {
   const emailRef = useRef()
   const passwordRef = useRef()
+
+  const handler = async(e) => {
+    e.preventDefault()
+  
+    const email = emailRef.current.value
+    const password =  passwordRef.current.value
+
+    console.log("Email:", email, "Password:", password); 
+    const result = await signIn("credentials",{
+      redirect: false,
+      email: email,
+      password: password,
+    })
+    console.log("SignIn result:", result);
+  
+    if (result.error) {
+      alert(result.error);
+    } else {
+      alert("Login successful!");
+      window.location.href = "/x-ray"; // Redirect to dashboard or another protected route
+    }
+  
+    //login input handle + navigation
+  }
   return (
     <div className={styles.loginContainer}>
       <h1 className={styles.title}>Welcome Back</h1>
       <button className={styles.googleLoginBtn}>Log in with Google</button>
       <div className={styles.separator}>Or</div>
+      <form onSubmit={handler}>
       <input
         type="email"
         placeholder="Enter your Email"
@@ -37,7 +61,8 @@ const Login = () => {
           Forgot Password?
         </a>
       </div>
-      <button onClick= {handler} className={styles.loginBtn}>Log in</button>
+      <button type="submit" className={styles.loginBtn}>Log in</button>
+      </form>
       <div className={styles.createAccount}>
         Not a member yet? <a href="#">Create an account</a>
       </div>
