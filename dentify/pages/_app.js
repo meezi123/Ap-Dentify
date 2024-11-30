65952caccfc10271f63b85d3f9a954e751a8abcb
rohazubair <144012283+rohazubair@users.noreply.github.com>
2024-11-30 13:48:18 +0500

    merged main

--- dentify/pages/_app.js
+++ dentify/pages/_app.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import "@/styles/globals.css";
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-=======
 import Navbar from "@/components/Navbar";
 import { SessionProvider } from "next-auth/react"
 import "@/styles/globals.css";
@@ -19,5 +13,4 @@ export default function App({ Component, pageProps }) {
     </>
   );
   
->>>>>>> 87cc7633beef45a1476c6d6d9f304d7b6f2c7792
 }
