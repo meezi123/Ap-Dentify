65f93813fc1d438c49577a2247457737b23eaa6c
Rameez <rameezqureshi818@gmail.com>
2024-12-01 22:39:02 +0500

    conflicts resolved

--- dentify/components/Navbar.jsx
+++ dentify/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'; 
+import Image from 'next/image';
 import Button from "./Button";
 import logo from '@/public/logo.png';
 import Link from 'next/link';
@@ -17,7 +17,7 @@ const Navbar = () => {
   const [showLogOut, setLogOut] = useState(false)
   const router = useRouter();
 
-  
+
 
   useEffect(() => {
     const checkSession = async () => {
@@ -33,18 +33,19 @@ const Navbar = () => {
     console.log("Navbar detected user context change:", user);
   }, [user]);
 
-  const logoutHandler= () => {
+  const logoutHandler = () => {
     signOut()
     alert("Successful LogOut !")
   }
-  const loginHandler = () =>{
+  const loginHandler = () => {
     setLogOut(false)
     router.push('/Login')
   }
   return (
     <>
       <nav style={{ borderRadius: '8px' }} className="w-full h-[90px] bg-lightBlue flex space-x-0 items-center justify-between ">
-      <div className="h-full w-[162px] flex items-center justify-center cursor-pointer">
+        {/* <div style={{ borderRadius: '8px' }} className="h-full w-[162px] items-center justify-center px-1 "> */}
+        <div className="h-full w-[162px] flex items-center justify-center cursor-pointer">
           <Image
             src={logo}
             alt="Logo"
@@ -53,24 +54,25 @@ const Navbar = () => {
             height={90}
             priority={true}
           />
-      </div>
+        </div>
+        {/* </div> */}
         <ul className='flex space-x-3 text-darkBlue font-sans'>
           <Link href='/' className='text-[16px] font-medium'>Home</Link>
           <Link href='/x-ray' className='text-[16px] font-medium'>X-Ray</Link>
           <Link href='/about' className='text-[16px] font-medium'>About</Link>
         </ul>
         <div className='flex items-center space-x-6 w-[280px] '>
-         <div className="h-full  items-center justify-center ">
-          <Link href={`/User/${user || "default"}`}>
-            <Image
-              src={userImage}
-              alt="user"
-              className="object-contain"
-              priority={true}
-            />
+          <div className="h-full  items-center justify-center ">
+            <Link href={`/User/${user || "default"}`}>
+              <Image
+                src={userImage}
+                alt="user"
+                className="object-contain"
+                priority={true}
+              />
             </Link>
           </div>
-          {showLogOut ? <Button text="Logout" onClick={logoutHandler}/> : <Button text='Log In' onClick={loginHandler}/>}
+          {showLogOut ? <Button text="Logout" onClick={logoutHandler} /> : <Button text='Log In' onClick={loginHandler} />}
         </div>
       </nav>
     </>
--- dentify/pages/_app.js
+++ dentify/pages/_app.js
@@ -1,18 +1,17 @@
 import Navbar from "@/components/Navbar";
-import { SessionProvider } from "next-auth/react"
-import { UserProvider } from '../context/UserContext';
+import { SessionProvider } from "next-auth/react";
+import { UserProvider } from "../context/UserContext";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return(
+  return (
     <>
-    <SessionProvider>
-      <UserProvider>
-        <Navbar/>
-        <Component {...pageProps} />
-      </UserProvider>
-    </SessionProvider>
+      <SessionProvider>
+        <UserProvider>
+          <Navbar />
+          <Component {...pageProps} />
+        </UserProvider>
+      </SessionProvider>
     </>
   );
-  
 }
